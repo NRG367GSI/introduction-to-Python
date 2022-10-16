15e00aff9930edfcfcc96e276b9d5a6f12bf12fa
NRG367GSI <NRG367GSI@gmail.com>
2022-10-16 23:07:31 +0300

    addad as changes

--- Homework_7_v_2/calc_complex_numbers.py
+++ Homework_7_v_2/calc_complex_numbers.py
@@ -1,4 +1,100 @@
-def operatyon(data):
-    return complex(eval(data))
-data = complex(input())
-print(operatyon(data))
+def condition(meaning):
+    if meaning == " ":
+        return True
+    else:
+        return False
+
+
+
+expression = "25+51i * (3+4i-5+6i)/(8+7i)"
+
+        
+
+def without_spaces(expression):
+    
+    new_expression = ""
+    separation = expression.split()
+    for i in separation:
+        new_expression += i
+    return new_expression
+
+def complex_eval(expression):
+    line = ""
+    separation = []
+    operation = "+-*/"
+
+    
+    if "(" in expression:
+        index_start = expression.find("(")
+        index_end = expression.find(")")
+        brackets = expression[index_start + 1:index_end]
+    print(expression[:index_start], expression[index_start + 1:index_end], expression[index_end + 1:], expression)
+  
+
+
+
+#print(complex_eval(without_spaces(expression)))
+
+def complex_num1(seq):
+    num_compl = []
+    
+    if "i" in seq:
+        index_i = seq.find("i")
+        index_plus = seq.find("+")
+        num_compl.append(int(seq[:index_plus]))
+        num_compl.append(int(seq[index_plus + 1:index_i]))
+        seq = seq[index_i + 1:]
+        print(num_compl, seq)
+    
+#complex_num(without_spaces(expression))
+
+def pars(seq):
+    operation = "+-*/()"
+    str = ""
+    arr = []
+    for i in range(len(seq)):
+        str += seq[i]
+        if seq[i] in operation:
+            str = str[:-1]
+            arr.append(str)
+            arr.append(seq[i])
+            str =""
+            
+    return arr
+
+#pars(without_spaces(expression))
+
+def complex_num(seq):
+    oper = "+-*/()"
+    operation = "+-"
+    lin = []
+    compl = []
+    for i in range(len(seq)):
+        
+        if seq[i] in operation and "i" in seq[i+1]:
+            compl.append(seq[i - 1])
+            
+            
+            seq[i + 1] = seq[i + 1][:-1]
+            compl.append(seq[i + 1])
+            lin.append(compl)
+            compl = []
+            del seq[i-1:i+1]
+    print(lin, seq)
+'''          
+            
+        if seq[i] in oper:
+            lin.append(seq[i])
+'''
+            
+
+
+complex_num(pars(without_spaces(expression)))
+
+
+
+
+        
+
+
+
--- Homework_7_v_2/menu.py
+++ Homework_7_v_2/menu.py
@@ -1,5 +1,49 @@
-import user_interface
+import calc_rational_numbers as rational
+import calc_complex_numbers as complex
+import loger as lg
+import user_interface as ui
 
-user_interface.menu()
+def menu():
+    menu = ["1", "2", "0"]
+    point = None
+    print("\n       Калькулятор запущен\n")
+    while point not in menu:
+        print("Меню:\n"
+              "1 - Калькулятор с рациональными числами\n"
+              "2 - Калькулятор с комплексными числами\n"
+              "3 - Просмотр истории\n"
+              "0 - Выход\n"
+              )
+        point = input("Введите, интерисующий пункт меню: ")
 
+        if point == "1":
+            print("\n-----------------------------------------------")
+            print("Выбран режим для работы с рациональными числами")
+            print("-----------------------------------------------\n")
+            input_data = ui.user_input()
+            result = rational.operatyon(input_data)
+            string_to_write = f"{input_data} = {str(result)}"
+            lg.write(string_to_write)
+            print(f"Ответ: {result}\n")
 
+        elif point == "2":
+            print("\n----------------------------------------------")
+            print("Выбран режим для работы с комплексными числами")
+            print("----------------------------------------------\n")
+            input_data = ui.user_input()
+            result = complex.operatyon(input_data)
+            string_to_write = f"{input_data} = {str(result)}"
+            lg.write(string_to_write)
+            print(f"Ответ: {result}")
+
+        elif point == "3":
+            lg.read()
+
+        elif point == "0":
+            break
+
+        else:
+            print("\n--------------------------------------")
+            print("Некорректный ввод, попробуйте еще раз!")
+            print("--------------------------------------\n")
+        point = None
