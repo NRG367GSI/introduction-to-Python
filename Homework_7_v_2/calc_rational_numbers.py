202f76e6a9c954148277d1d1792ce618bb90ac4a
NRG367GSI <NRG367GSI@gmail.com>
2022-10-18 21:42:19 +0300

    Сделал вещественные числа в калькуляторе

--- Homework_7_v_2/calc_rational_numbers.py
+++ Homework_7_v_2/calc_rational_numbers.py
@@ -11,10 +11,12 @@ def calc(expression):
             return calc(expression[:expression.find('*')]) * calc(expression[expression.find('*')+1:])
         if expression.find('/') >=0: #if '+' in stroka
             return calc(expression[:expression.find('/')]) / calc(expression[expression.find('/')+1:])
-        return int(expression)
+        return float(expression)
     except NameError:
         return "Неправильный запрос (400)"
     except ZeroDivisionError:
         return "Деление на ноль"
+    except ValueError:
+        return "ValueError"
 
 print(calc(input("введите выражение")))#ui.user_input()))
