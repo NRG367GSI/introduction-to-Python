7be10ab53f2018ff668bb9a53b0f5e9d3f6a9ed9
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-11-11 00:29:00 +0500

    удалил не нужные файлы, создал кнопки на команду search

--- Seminar_10/search_data.py
+++ Seminar_10/search_data.py
@@ -1,52 +1,49 @@
 import working_with_database as wwd
-def search_name(sur_name, name):
-    collection = []
-    data = wwd.open_file()
-    if data != None:
-        for i in data:
-            if i["surname"] == sur_name and i["name"] == name: #sur_name in i['surname'] and name in i["name"]:#
-                collection.append(i)
-                return collection
-    else:
-        return None
-            
 
-def search_birthday(day):
-    collection = []
-    data = wwd.open_file()
+
+def search_name_in_db(name):
+  collection = []
+  data = wwd.open_file()
+  if data != None:
     for i in data:
-        if day == i["hb"]:
-            collection.append(i)
-    if len(collection) != 0:
+      if i["name"] == name:
+        collection.append(i)
         return collection
-    else:
-        return None
-    
-        
+  else:
+    return None
 
 
-def search_class(grade):
-    collection = []
-    data = wwd.open_file()
-    for i in data:
-        if grade in i["class"]:
-            collection.append(i)
-    if len(collection) != 0:
-        return collection
-    else:
-        return None
+def search_birthday_in_db(day):
+  collection = []
+  data = wwd.open_file()
+  for i in data:
+    if day == i["hb"]:
+      collection.append(i)
+  if len(collection) != 0:
+    return collection
+  else:
+    return None
 
 
-def search_status(status):
-    collection = []
-    data = wwd.open_file()
-    for i in data:
-        if status in i["status"]:
-            collection.append(i)
-    if len(collection) != 0:
-        return collection
-    else:
-        return None
+def search_class_in_db(grade):
+  collection = []
+  data = wwd.open_file()
+  for i in data:
+    if grade in i["class"]:
+      collection.append(i)
+  if len(collection) != 0:
+    return collection
+  else:
+    return None
 
 
-
+def search_status_in_db(status):
+  collection = []
+  data = wwd.open_file()
+  for i in data:
+    if status in i["status"]:
+      collection.append(i)
+  if len(collection) != 0:
+    return collection
+  else:
+    return None
